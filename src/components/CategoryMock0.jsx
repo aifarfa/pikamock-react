5a1465e38ee2db3d9ba83269ee7b8b68229eeb92
Parnaigon Yangpaksi <JD@Parnaigons-MacBook-Pro.local>
2016-10-09 19:54:46 +0700

    Add category page and new images

--- src/components/CategoryMock0.jsx
+++ src/components/CategoryMock0.jsx
@@ -8,7 +8,62 @@ export default class CategoryMock0 extends React.Component {
 
   render() {
     return (
-      <div className="container pikapok__content">
+      <div classNameName="container pikapok__content">
+
+        <div id="pikapok__carousel" className="carousel slide pikapok__carousel" data-ride="carousel">
+          <div classNameName='row'>
+            <div className='col-xs-12'>
+              <h3 className='categories-header'>EXQUISITE ACCESSORY</h3>
+            </div>
+          </div>
+          <div className='row'>
+             <div className="col-xs-12 col-sm-4 thumbnail">
+                <img src="img/EA01.jpg" alt="" className="sigle-img" width="380" height="373" />
+                <div className="caption text-center">
+                  <p>“Roses of Love”</p>
+                  <p>Code : EA 1</p>
+                  <p>
+                    <a href="#" className="btn btn-info" role="button">PICK NOW</a>
+                  </p>
+                </div>
+             </div>
+
+             <div className="col-xs-12 col-sm-4 thumbnail">
+                <img src="img/EA02.jpg" alt="" className="single-img" width="380" height="373" />
+                <div className="caption text-center">
+                  <p>“Roses of Love”</p>
+                  <p>Code : EA 2</p>
+                  <p>
+                    <a href="#" className="btn btn-info" role="button">PICK NOW</a>
+                  </p>
+                </div>
+             </div>
+
+             <div className="col-xs-12 col-sm-4 thumbnail">
+                <img src="img/EA03.jpg" alt="" className="single-img" width="380" height="373" />
+                <div className="caption text-center">
+                  <p>“Ruby of Luck”</p>
+                  <p>Code : EA 3</p>
+                  <p>
+                    <a href="#" className="btn btn-info" role="button">PICK NOW</a>
+                  </p>
+                </div>
+             </div>
+
+             <div className="col-xs-12 col-sm-4 thumbnail">
+                <img src="img/EA04.jpg" alt="" className="single-img" width="380" height="373" />
+                <div className="caption text-center">
+                  <p>“Forever Rose”</p>
+                  <p>Code : EA 4</p>
+                  <p>
+                    <a href="#" className="btn btn-info" role="button">PICK NOW</a>
+                  </p>
+                </div>
+             </div>
+
+
+             </div>
+           </div>
       </div>
     ); // return
   }
