1a500644fc8253ce318464d3c75d9707cb41cd3f
pasit.r <kzneko@gmail.com>
2016-10-09 19:39:20 +0700

    add build config for prod

---
+++ webpack.config.prod.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const APP_DIR = path.resolve('./src');
+const BUILD_DIR = path.resolve('./public');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = config = {
+
+  entry: [
+    'babel-polyfill',
+    APP_DIR + '/index.jsx'
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      include: APP_DIR,
+      loader: 'react-hot!babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader?importLoaders=1'
+    }, {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000'
+    }]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+    // new webpack.HotModuleReplacementPlugin(),
+    // new ExtractTextPlugin('style.css', {
+    //   allChunks: true,
+    //   disable: true
+    // })
+  ]
+};
