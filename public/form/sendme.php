3ec624044bcaaffc9aa3ff0a29897b9d514c89b4
pasit.r <kzneko@gmail.com>
2016-10-22 15:48:21 +0700

    catch exception

--- public/form/sendme.php
+++ public/form/sendme.php
@@ -1,21 +1,25 @@
 <?php
 
-if ( $_SERVER['REQUEST_METHOD'] != 'POST' ) {
-  http_response_code(403);
-  return;
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    http_response_code(403);
+    return;
 }
 
-if(!empty($_POST)){
-  var_dump($_POST);
+if (!empty($_POST)) {
+    var_dump($_POST);
 
-  $to = 'kzneko@gmail.com';
-  $subject = 'hello it works';
-  $headers = 'from: official@pikapok.com' . "\r\n" .
-    'Reply-To: official@pikapok.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+    $to = 'kzneko@gmail.com';
+    $subject = 'hello it works';
+    $headers = 'From: official@pikapok.com'."\n".
+    'X-Mailer: PHP/'.phpversion();
 
-  mail($to, $subject, $message, $headers);
-}
-else {
-  echo 'empty body';
+    try {
+        mail($to, $subject, $message, $headers);
+        echo 'mail sent.';
+    } catch (Exception $e) {
+        echo 'Sorry, something went wrong. please try again later.';
+        var_dump($e);
+    }
+} else {
+    http_response_code(400); // Bad request
 }
