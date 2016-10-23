20475497118df414a53956c6231c0ac6f8c71746
pasit.r <kzneko@gmail.com>
2016-10-23 22:42:41 +0700

    add swiftmailer

--- public/form/sendme.php
+++ public/form/sendme.php
@@ -1,25 +1,66 @@
 <?php
 
+require_once dirname(__FILE__).'/../../vendor/autoload.php';
+
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    http_response_code(403);
-    return;
+    $swift_ready = class_exists('Swift_SmtpTransport', true);
+
+    http_response_code(200);
+    echo 'OK.'.$swift_ready;
+    exit();
 }
 
-if (!empty($_POST)) {
-    var_dump($_POST);
-
-    $to = 'kzneko@gmail.com';
-    $subject = 'hello it works';
-    $headers = 'From: official@pikapok.com'."\n".
-    'X-Mailer: PHP/'.phpversion();
-
-    try {
-        mail($to, $subject, $message, $headers);
-        echo 'mail sent.';
-    } catch (Exception $e) {
-        echo 'Sorry, something went wrong. please try again later.';
-        var_dump($e);
-    }
-} else {
+if (empty($_POST)) {
     http_response_code(400); // Bad request
+    exit();
 }
+
+// Port 25
+// TLS not required
+// Dynamic IPs allowed
+// Mail can only be sent to Gmail or G Suite users
+$transport = Swift_SmtpTransport::newInstance('aspmx.l.google.com', 25)
+  ->setUsername('official@pikapok.com')
+  ->setPassword('0kpakip.com');
+
+$mailer = Swift_Mailer::newInstance($transport);
+
+$message = Swift_Message::newInstance('Test Subject')
+  ->setFrom(array('official@pikapok.com' => 'Pikapok'))
+  ->setTo(array('official@pikapok.com'))
+  ->setBody('message sent via aspmx.l.google.com');
+
+try {
+    $result = $mailer->send($message);
+    var_dump($result);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+// $from = 'Pikapok <official@pikapok.com>';
+// $to = 'kzneko@gmail.com';
+// $subject = 'Hello! it works!';
+// $body = "Foo,\n\nbar you?";
+//
+// $headers = array(
+//     'From' => $from,
+//     'To' => $to,
+//     'Subject' => $subject,
+// );
+//
+// $smtp = Mail::factory('smtp', array(
+//     'host' => 'ssl://smtp.gmail.com',
+//     'port' => '465',
+//     'auth' => true,
+//     'username' => 'official@pikapok.com',
+//     'password' => '0kpakip.com',
+// ));
+//
+// $mail = $smtp->send($to, $headers, $body);
+//
+// if (PEAR::isError($mail)) {
+//     echo '<p>'.$mail->getMessage().'</p>';
+// } else {
+//     // echo $sent ? 'mail sent.' : 'Sorry, something went wrong. please try again later.';
+//     echo '<p>Message successfully sent!</p>';
+// }
